7e343bbe9c62c197fe46ba93be08e62d2e546761
Matteo Casarin <casarinmatteo22@gmail.com>
2018-04-02 18:10:05 +0200

    added delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,56 @@
+// SQL vs noSQL
+// table vs collection/array
+// row vs document/object
+// column vs field/property
+
+//WORKS FOR MONGODB VERSION 2.X.X
+
+//server must be running;
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos')
+  //   .deleteMany({text: 'eat lunch'})
+  //   .then((result) => {
+  //     console.log(result);
+  //   });
+
+  // db.collection('Todos')
+  //   .deleteOne({text: 'eat lunch'})
+  //   .then((result) => {
+  //     console.log(result);
+  //   });
+
+  // db.collection('Todos')
+  //   .findOneAndDelete({completed: false})
+  //   .then((result) => {
+  //     console.log(result);
+  //   });
+
+  db.collection('Users').count({name: 'Matteo'}).then((count) => {
+    if(count > 1) {
+      db.collection('Users').deleteMany({name: 'Matteo'});
+      console.log('deleted something');
+    } else {
+      console.log('found no duplicates');
+    };
+  });
+
+  db.collection('Users')
+    .findOneAndDelete({
+      _id: new ObjectID('5ab409e1f336731147b60e2e')
+    })
+    .then((result) => {
+      console.log(result);
+    });
+
+
+
+   //db.close();
+});
