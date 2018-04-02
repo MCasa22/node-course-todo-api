8e323ce757d5e0a738ea5d6b73ede5debeafb786
Matteo Casarin <casarinmatteo22@gmail.com>
2018-04-02 18:33:51 +0200

    added update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,46 @@
+// SQL vs noSQL
+// table vs collection/array
+// row vs document/object
+// column vs field/property
+
+//WORKS FOR MONGODB VERSION 2.X.X
+
+//server must be running;
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos')
+  //   .findOneAndUpdate({
+  //     _id: new ObjectID('5ac2415600390a8fc31c9e33')
+  //   }, {
+  //     $set: {
+  //       completed: true
+  //     }
+  //   }, {
+  //     returnOriginal: false
+  //   })
+  //   .then((result) => {
+  //     console.log(result);
+  //   });
+
+  db.collection('Users')
+    .findOneAndUpdate({
+      _id: new ObjectID('5ab3fbdcf0789b0f2cedb82d')
+    }, {
+      $set: {name: 'Matteo'},
+      $inc: {age: 1}
+    }, {
+      returnOriginal: false
+    })
+    .then((result) => {
+      console.log(result);
+    })
+
+   //db.close();
+});
