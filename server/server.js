31f6adf6216f4dc532a0e3341978df1fe232d5e4
Matteo Casarin <casarinmatteo22@gmail.com>
2018-04-04 21:35:03 +0200

    added GET /todos route

--- server/server.js
+++ server/server.js
@@ -24,7 +24,15 @@ app.post('/todos', (req, res) => {
     });
 });
 
-
+app.get('/todos', (req, res) => {
+  Todo.find()
+    .then((todos) => {
+      res.send({todos});
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    })
+});
 
 app.listen(3000, () => {
   console.log('server started in port 3000');
