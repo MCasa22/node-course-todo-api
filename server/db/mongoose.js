4fa38f3f983bf06b488e796dadcca3f99266f019
Matteo Casarin <casarinmatteo22@gmail.com>
2018-04-08 16:03:50 +0200

    setup new connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,7 +6,7 @@ let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
   mlab: 'mongodb://dbUser:qwertyasdf@ds145438.mlab.com:45438/todo-api-db'
 };
-mongoose.connect(db.mlab || db.localhost);
+mongoose.connect( process.env.PORT ? db.mlab : db.localhost);
 
 
 module.exports = {
