a275962f6b76ea71936615a7d9e88815c5c19007
Matteo Casarin <casarinmatteo22@gmail.com>
2018-04-27 12:03:55 +0200

    added hashing logic for pw

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,11 +1,28 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let data = {
-  id: 10
-}
+let password = 'abc123!';
 
-let token = jwt.sign(data, 'salting the hash');
-console.log('TOKEN', token);
+//generate encrypted and salted password
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt. hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
 
-let decoded = jwt.verify(token, 'salting the hash');
-console.log('DECODED', decoded);
+//check if hashedPassword is the actual password
+let hashedPassword = '$2a$10$hTMlqChl/D4PME.cdm8vsewGJ3STe2TPAFl13AwyIc5OUpTQoUj1e';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log('res: ', res);
+});
+
+// let data = {
+//   id: 10
+// }
+//
+// let token = jwt.sign(data, 'salting the hash');
+// console.log('TOKEN', token);
+//
+// let decoded = jwt.verify(token, 'salting the hash');
+// console.log('DECODED', decoded);
